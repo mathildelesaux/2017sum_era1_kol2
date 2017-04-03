1f0bf7b323bb25cf92f2a2175f64a3b78514c488
mathildelesaux <mathildelesaux@orange.fr>
2017-04-03 11:51:35 +0200

    Update kol2.py

--- kol2.py
+++ kol2.py
@@ -16,16 +16,33 @@
 # If you have even more courage, try implementing user interface.
 
 
-
 class diary(object):
+    nb_stud = 0
+    def __init__(self,name="unknown",surname="unknown",marks=None):
+        self.name=name
+        self.surname=surname
+        self.marks=marks
+        diary.nb_stud =+1
+        
+    def display(self):
+        print(self.name,"-",self.surname,"-",self.marks)
+        
+    def nb_stud(self):
+        print("There are",diary.nb_stud,"students")
+        
+    def average_all_student(marks):
+        for i in marks : 
+            average = sum(i)/len(marks)
+        print(average)
 
-def __init__(self,name="unknown",surname="unknown",marks=None):
-	self.name=name
-	self.surname=surname
-	self.marks=marks
-
-def average_all_student(marks):
-	average = 0
-	for i in marks :
-		
-
+stud1=diary("Martin","Maurice",12)
+stud1.display()
+print()
+stud2=diary("Lucy","MacCafé",4)
+stud2.display()
+print()
+stud3=diary("Yves","StLaurent",18)
+stud3.display()
+print()
+stud1.average_all_student()
+print()
